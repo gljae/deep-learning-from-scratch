efd402de9c9df2ec97e83ebd184117c509c52d7c
gijae05 <91142899+gljae@users.noreply.github.com>
2026-03-11 14:10:46 +0000

    4.4-4.6

--- common/functions.py
+++ common/functions.py
@@ -1,11 +1,39 @@
 import numpy as np
 
+
+def identity_function(x):
+    return x
+
+
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+
 def sigmoid(x):
-    return 1/(1+ + np.exp(-x))
+    return 1 / (1 + np.exp(-x))    
+
+
+def sigmoid_grad(x):
+    return (1.0 - sigmoid(x)) * sigmoid(x)
+    
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def relu_grad(x):
+    grad = np.zeros(x)
+    grad[x>=0] = 1
+    return grad
+    
 
 def softmax(x):
     x = x - np.max(x, axis=-1, keepdims=True) # 오버플로 대책
     return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
+
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y-t)**2)
+
 
 def cross_entropy_error(y, t):
     if y.ndim == 1:
@@ -18,3 +46,8 @@ def cross_entropy_error(y, t):
              
     batch_size = y.shape[0]
     return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+
+def softmax_loss(X, t):
+    y = softmax(X)
+    return cross_entropy_error(y, t)
